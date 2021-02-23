d064d7b1b8c90bc945ed64446c2e607743740846
Sam12604 <samharris12106@gmail.com>
2021-02-23 14:53:23 +0000

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var id
 var keyword
 
 async function getData() {
-	response1 = await fetch('https://sam12604.github.io/randommovie/keywords.json');
+	response1 = await fetch('https://randommoive.samharris.xyz/keywords.json');
 	api_data1 = await response1.json()
 
 	random_num = getRandomInt(0,api_data1.length-1)
